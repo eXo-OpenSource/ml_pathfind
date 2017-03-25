bae1e30d390ba6f45338e40798e34ffed2980cce
Jusonex <jusonex96@gmail.com>
2017-03-25 15:18:10 +0100

    Link against shared Lua on Linux

--- module/premake5.lua
+++ module/premake5.lua
@@ -20,11 +20,14 @@ project "module"
 		"**.h"
 	}
 	
-	filter { "system:windows", "platforms:x86" }
-		links { "lua5.1.lib" }
-		
-	filter { "system:windows", "platforms:x64" }
-		links { "lua5.1_64.lib" }
+	filter {"system:linux", "platforms:x86" }
+			linkoptions { "-Wl,-rpath=mods/deathmatch" }
 
-	filter "system:not windows"
+	filter {"system:linux", "platforms:x64" }
+		linkoptions { "-Wl,-rpath=x64" }
+
+	filter "system:linux"
+		linkoptions { "-l:lua5.1.so" }
+
+	filter "system:windows"
 		links { "lua5.1" }
